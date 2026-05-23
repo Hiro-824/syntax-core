8091896e3682ffddbd2db3af33e898eda613afbe
Hiro-824 <thefilepreserver@gmail.com>
2026-05-23 21:21:20 +0900

    Enhance HPSG implementation by adding new lexical entry types and rules for nouns and verbs. Update index exports to include ConstantLexemeInput and related types, improving the handling of lexical entries in the parser.

--- src/grammars/hpsg/hpsg.ts
+++ src/grammars/hpsg/hpsg.ts
@@ -1,9 +1,60 @@
 import { BinaryRules } from "../../core/parser.js";
 import { FeatureStructure, FeatureStructureInput } from "../../features/features.js";
 import { TypeSystem } from "../../features/types.js";
+import { applyConstantLexemeLexicalRule } from "./generation/const-rules.js";
+import {
+    applyPluralNounLexicalRule,
+    applySingularNounLexicalRule,
+} from "./generation/noun-rules.js";
+import { buildCompleteLexeme } from "./generation/type-constraint-applier.js";
+import {
+    applyBaseFormLexicalRule,
+    applyNonThirdSingularVerbLexicalRule,
+    applyPastParticipleLexicalRule,
+    applyPastTenseVerbLexicalRule,
+    applyPresentParticipleLexicalRule,
+    applyThirdSingularVerbLexicalRule,
+} from "./generation/verb-rules.js";
+import {
+    CountNounLexemeInput,
+    LexemeInput,
+    MassNounLexemeInput,
+} from "./lexemes/types.js";
 import { applyHpsgPrinciples } from "./principles/index.js";
 import { ruleData, RuleDefinition } from "./rules.js";
 import { typeDefinition } from "./types.js";
+
+export type VerbLexemeInput = Extract<
+    LexemeInput,
+    { type: "siv-lxm" | "piv-lxm" | "stv-lxm" | "dtv-lxm" | "ptv-lxm" }
+>;
+
+export type ConstantLexemeInput = Extract<
+    LexemeInput,
+    { type: "pn-lxm" | "pron-lxm" | "adj-lxm" | "adv-lxm" | "det-lxm" | "argmkp-lxm" | "predp-lxm" | "part-lxm" }
+>;
+
+export type CountNounWords = {
+    singular: FeatureStructure;
+    plural: FeatureStructure;
+};
+
+export type MassNounWords = {
+    singular: FeatureStructure;
+};
+
+export type VerbWords = {
+    base: FeatureStructure;
+    nonThirdSingular: FeatureStructure;
+    thirdSingular: FeatureStructure;
+    presentParticiple: FeatureStructure;
+    pastTense: FeatureStructure;
+    pastParticiple: FeatureStructure;
+};
+
+export type ConstantWords = {
+    word: FeatureStructure;
+};
 
 export class HPSGLexicalEntryCompiler {
     constructor(private types: TypeSystem) {}
@@ -159,5 +210,79 @@ export class HPSG {
 
     compileLexicalEntries(inputs: FeatureStructureInput[]): FeatureStructure[] {
         return this.lexicalEntryCompiler.compileMany(inputs);
+    }
+
+    buildLexeme(input: LexemeInput): FeatureStructure {
+        return buildCompleteLexeme(input, this.types);
+    }
+
+    applySingularNounRule(lexeme: FeatureStructure): FeatureStructure {
+        return applySingularNounLexicalRule(lexeme, this.types);
+    }
+
+    applyPluralNounRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyPluralNounLexicalRule(lexeme, this.types);
+    }
+
+    applyBaseVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyBaseFormLexicalRule(lexeme, this.types);
+    }
+
+    applyNonThirdSingularVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyNonThirdSingularVerbLexicalRule(lexeme, this.types);
+    }
+
+    applyThirdSingularVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyThirdSingularVerbLexicalRule(lexeme, this.types);
+    }
+
+    applyPresentParticipleVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyPresentParticipleLexicalRule(lexeme, this.types);
+    }
+
+    applyPastTenseVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyPastTenseVerbLexicalRule(lexeme, this.types);
+    }
+
+    applyPastParticipleVerbRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyPastParticipleLexicalRule(lexeme, this.types);
+    }
+
+    applyConstantRule(lexeme: FeatureStructure): FeatureStructure {
+        return applyConstantLexemeLexicalRule(lexeme, this.types);
+    }
+
+    buildCountNounWords(input: CountNounLexemeInput): CountNounWords {
+        const lexeme = this.buildLexeme(input);
+        return {
+            singular: this.applySingularNounRule(lexeme),
+            plural: this.applyPluralNounRule(lexeme),
+        };
+    }
+
+    buildMassNounWords(input: MassNounLexemeInput): MassNounWords {
+        const lexeme = this.buildLexeme(input);
+        return {
+            singular: this.applySingularNounRule(lexeme),
+        };
+    }
+
+    buildVerbWords(input: VerbLexemeInput): VerbWords {
+        const lexeme = this.buildLexeme(input);
+        return {
+            base: this.applyBaseVerbRule(lexeme),
+            nonThirdSingular: this.applyNonThirdSingularVerbRule(lexeme),
+            thirdSingular: this.applyThirdSingularVerbRule(lexeme),
+            presentParticiple: this.applyPresentParticipleVerbRule(lexeme),
+            pastTense: this.applyPastTenseVerbRule(lexeme),
+            pastParticiple: this.applyPastParticipleVerbRule(lexeme),
+        };
+    }
+
+    buildConstantWords(input: ConstantLexemeInput): ConstantWords {
+        const lexeme = this.buildLexeme(input);
+        return {
+            word: this.applyConstantRule(lexeme),
+        };
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@ export type { BinaryRules, Node, TerminalEntry, TerminalRules } from "./core/par
 
 export { CFG, createCFGTerminalRules } from "./grammars/cfg/cfg.js";
 export { HPSG, HPSGBinaryRules, HPSGLexicalEntryCompiler } from "./grammars/hpsg/hpsg.js";
+export type {
+    ConstantLexemeInput,
+    ConstantWords,
+    CountNounWords,
+    MassNounWords,
+    VerbLexemeInput,
+    VerbWords,
+} from "./grammars/hpsg/hpsg.js";
+export type { LexemeInput } from "./grammars/hpsg/lexemes/types.js";
